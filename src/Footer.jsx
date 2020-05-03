af467d53c21d22471ee3ae910c63d6807321b5ee
aradhna7 <aradhna.7sharma@gmail.com>
2020-05-04 01:17:27 +0530

    keeper project part 1

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+    const d=new Date();
+    const y=d.getFullYear();
+   return <footer className="footer"><p className="footer p">Copyright @ {y}</p></footer>;
+}
+
+export default Footer;
---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Note(){
+    return <div className="note">
+        <h1 className="h1">this is the note title</h1>
+        <p className="p">this is the note title</p>
+    </div>
+    ;
+}
+
+export default Note;
